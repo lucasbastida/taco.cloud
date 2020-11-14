5924997f5225d127d8c9027cd2e4cb70cf1caadc
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2020-11-14 11:55:13 -0300

    basic home.html and test

---
+++ src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired //The test class is injected with a MockMvc object for the test to drive the mockup.
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")));
+    }
+
+}
